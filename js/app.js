4f8f74cb2ec1b4374f16d0cb76b9964a4f91ec4d
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-19 13:56:38 +0100

    Add helper methods to get and create elements and refactored code to use these

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("#new-item-form");
+    const form = getElement("#new-item-form");
     form.addEventListener("submit", handleFormSubmit);
 
-    const deleteDiv = document.querySelector("#below-list");
-    const deleteButton = document.createElement("button");
+    const deleteDiv = getElement("#below-list");
+    const deleteButton = createElement("button");
     deleteButton.textContent = "Delete All";
     deleteButton.setAttribute("type", "button");
     deleteDiv.appendChild(deleteButton);
@@ -12,13 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const handleFormSubmit = function(event) {
-    console.log(event);
-    
-    event.preventDefault();
-    const stepLog = document.querySelector("#step-log");
-    const listItem = document.createElement("li");
-    const steps = document.createElement("div");
-    const date = document.createElement("div");
+event.preventDefault();
+    const stepLog = getElement("#step-log");
+    const listItem = createElement("li");
+    const steps = createElement("div");
+    const date = createElement("div");
     steps.setAttribute("id", "steps");
     date.setAttribute("id", "date")
     steps.textContent = `Steps: ${event.target.steps.value}`;
@@ -29,8 +27,16 @@ const handleFormSubmit = function(event) {
     resetForm();
 };
 
+const getElement = function(identifier) {
+    return document.querySelector(identifier);
+} 
+
+const createElement = function(element) {
+    return document.createElement(element);
+};
+
 const resetForm = function() {
-    document.querySelector("#new-item-form").reset();
+    getElement("#new-item-form").reset();
 };
 
 const handleDeleteClick = function() {
